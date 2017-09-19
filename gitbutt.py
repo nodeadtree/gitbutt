373b2589266ca0f331ff0fad552c4f9c9ec120cf
Juniper Overbeck <nodeadtree@gmail.com>
2017-09-18 18:54:42 -0700

    2017-09-18 18:54:42

--- gitbutt.py
+++ gitbutt.py
@@ -1,6 +1,8 @@
 import subprocess
 import datetime
 import os
+import time
+time.sleep(30)
 path = os.path.dirname(os.path.realpath(__file__)) + '/gitbutt.py'
 message = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
 with open(path, 'r') as f:
@@ -11,4 +13,4 @@ with open(path, 'w') as f:
         f.write(i)
 subprocess.run(['git', 'commit', '-a', '-m', message])
 subprocess.run(['git', 'push', 'origin', 'master'])
-#2017-09-18 18:37:57
+#2017-09-18 18:54:42
