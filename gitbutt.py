192c6f0ffad155a36387c2b93c3606f9830ff66e
Juniper Overbeck <nodeadtree@gmail.com>
2017-09-18 18:11:58 -0700

    2017-09-18 18:11:58

--- gitbutt.py
+++ gitbutt.py
@@ -1,2 +1,7 @@
-import os
-:
+import subprocess
+import datetime
+
+
+message = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+subprocess.run(['git', 'commit', '-a', '-m', message])
+subprocess.run(['git', 'push', 'origin', 'master'])
