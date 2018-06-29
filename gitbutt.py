4d4e35f5e65b30133cbf4fd31c8ab980740e7ffd
Juniper Overbeck <nodeadtree@gmail.com>
2018-06-29 13:49:52 -0700

    Added License

--- gitbutt.py
+++ gitbutt.py
@@ -2,6 +2,11 @@ import subprocess
 import datetime
 import os
 import time
+# Copyright (c) 2018 Juniper Overbeck
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 time.sleep(30)
 path = os.path.dirname(os.path.realpath(__file__)) + '/gitbutt.py'
 message = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
