01e42ba7ed72a089ed4b2e86397fdd1ed9a7d379
Juniper Overbeck <nodeadtree@gmail.com>
2019-01-14 20:36:56 -0800

    2019-01-14 20:36:56

--- gitbutt.py
+++ gitbutt.py
@@ -18,4 +18,4 @@ with open(path, 'w') as f:
         f.write(i)
 subprocess.run(['git', 'commit', '-a', '-m', message])
 subprocess.run(['git', 'push', 'origin', 'master'])
-#2019-01-13 02:48:07
+#2019-01-14 20:36:56
